5d5d0d4ab2383c4c4a7317876ec921727c008151
shepai <dextershepherd@hotmail.co.uk>
2022-04-02 12:26:21 +0100

    Update realtime.py

--- Dev/Testing/realtime.py
+++ Dev/Testing/realtime.py
@@ -7,9 +7,12 @@ array = np.random.normal(0, 0.5, (100))
 while True:
     plt.cla()
     plt.plot(array)
-    array+=np.random.normal(0, 0.5, (100))
+    array+=np.random.normal(0, 0.1, (100))
     array[array<-4]=-4
     array[array>4]=4
+    plt.ylim(bottom=-4,top=4)
+    plt.title("Gaussian distribution mutation over time")
+    plt.xlabel("Index")
+    plt.ylabel("Value")
     plt.pause(0.1)
-    plt.ylim(ymax = -4, ymin = 4)
     plt.show(block=False)

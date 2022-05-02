b9dbf8546e173502c8c0925e9aa3d25b3b4d5ad8
shepai <dextershepherd@hotmail.co.uk>
2022-05-02 13:31:03 +0100

    reorg

---
+++ Dev/BackBending/agent.py
@@ -0,0 +1,188 @@
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy import misc
+
+import numpy as np
+import copy
+import torch
+
+class Agent_defineLayers:
+    def __init__(self, num_input, layers, num_output):
+        assert type(layers)==type([]), "Error with layers, give array of the number of layers"
+        self.num_input = num_input  #set input number
+        self.num_output = num_output #set ooutput number
+        self.hidden=[]
+        last=num_input
+        self.num_genes=0
+        for layer in layers:
+            self.hidden.append(layer)
+            self.num_genes+=(last * layer)
+            last=layer
+        self.num_genes +=(self.hidden[-1]*num_output)+num_output
+        self.weights = None
+        self.hidden_weights=None
+        self.bias = None
+        print("Auto",self.num_genes)
+    def set_genes(self, gene):
+        weight_idxs = self.num_input * self.hidden[0] #size of weights to hidden
+        current=weight_idxs
+        weights_idxs=[current] #start with end of last
+        for i in range(len(self.hidden)-1):
+            current+=self.hidden[i]*self.hidden[i+1] #calculate next idx for each layer
+            weights_idxs.append(current)
+        bias_idxs=None
+        weights_idxs.append(self.hidden[-1] * self.num_output + weights_idxs[-1]) #add last layer heading to output
+        bias_idxs = weights_idxs[-1]+ self.num_output #sizes of biases
+        w = gene[0 : weight_idxs].reshape(self.hidden[0], self.num_input)   #merge genes
+        ws=[]
+        for i in range(len(self.hidden)-2):
+            ws.append(gene[weights_idxs[i] : weights_idxs[i+1]].reshape(self.hidden[i+1], self.hidden[i]))
+        ws.append(gene[weights_idxs[-2] : weights_idxs[-1]].reshape(self.num_output, self.hidden[-1]))
+        b = gene[weights_idxs[-1]: bias_idxs].reshape(self.num_output,) #merge genes
+
+        self.weights = torch.from_numpy(w) #assign weights
+        self.hidden_weights=[]
+        for w in ws:
+            self.hidden_weights.append(torch.from_numpy(w))
+        self.bias = torch.from_numpy(b) #assign biases
+
+    def forward(self, x):
+        x = torch.from_numpy(x.flatten()/255).unsqueeze(0)
+        x = torch.mm(x, self.weights.T) #first layer
+        for i in range(len(self.hidden_weights)-1):
+            x =torch.mm(x,self.hidden_weights[i].T) #second layer
+        return torch.mm(x,self.hidden_weights[-1].T) + self.bias #third layer
+        
+    def get_action(self, x):
+        #vectors=[(1,1),(1,0),(0,1),(-1,-1),(-1,0),(0,-1),(-1,1),(1,-1)] #possible moves
+        arr=list(self.forward(x)[0])
+        ind=np.argmax(arr)
+        return ind
+
+class Agent_Conv:
+    def __init__(self, num_input, layers, num_output):
+        assert type(layers)==type([]), "Error with layers, give array of the number of layers"
+        self.num_input = num_input  #set input number
+        self.num_output = num_output #set ooutput number
+        self.hidden=[]
+        last=num_input
+        self.num_genes=0
+        for layer in layers:
+            self.hidden.append(layer)
+            self.num_genes+=(last * layer)
+            last=layer
+        self.num_genes +=(self.hidden[-1]*num_output)+num_output
+        self.weights = None
+        self.hidden_weights=None
+        self.bias = None
+        print("Auto",self.num_genes)
+    def set_genes(self, gene):
+        weight_idxs = self.num_input * self.hidden[0] #size of weights to hidden
+        current=weight_idxs
+        weights_idxs=[current] #start with end of last
+        for i in range(len(self.hidden)-1):
+            current+=self.hidden[i]*self.hidden[i+1] #calculate next idx for each layer
+            weights_idxs.append(current)
+        bias_idxs=None
+        weights_idxs.append(self.hidden[-1] * self.num_output + weights_idxs[-1]) #add last layer heading to output
+        bias_idxs = weights_idxs[-1]+ self.num_output #sizes of biases
+        w = gene[0 : weight_idxs].reshape(self.hidden[0], self.num_input)   #merge genes
+        ws=[]
+        for i in range(len(self.hidden)-1):
+            ws.append(gene[weights_idxs[i] : weights_idxs[i+1]].reshape(self.hidden[i+1], self.hidden[i]))
+        ws.append(gene[weights_idxs[-2] : weights_idxs[-1]].reshape(self.num_output, self.hidden[-1]))
+        b = gene[weights_idxs[-1]: bias_idxs].reshape(self.num_output,) #merge genes
+
+        self.weights = torch.from_numpy(w) #assign weights
+        self.hidden_weights=[]
+        for w in ws:
+            self.hidden_weights.append(torch.from_numpy(w))
+        self.bias = torch.from_numpy(b) #assign biases
+
+    def forward(self, x):
+        #create conv layer
+        m = torch.nn.Conv1d(len(x), len(self.weights), 3, stride=2,padding=1)
+        #Format input
+        H=len(x)
+        x = np.reshape(torch.from_numpy(x.flatten()/255).unsqueeze(0),(1,H,1))
+        #place through network conv layer
+        x = m(torch.tensor(x.float()))
+        x = torch.tensor(np.dot(self.weights.T.float(),x.detach().numpy()).flatten())
+        #run through forward layers
+        x = torch.mm(x.reshape(1,H), self.weights.T.float()) #first layer
+        for i in range(len(self.hidden_weights)-1):
+            x =torch.mm(x,self.hidden_weights[i].T.float()) #second layer
+        return torch.mm(x,self.hidden_weights[-1].T.float()) + self.bias #third layer
+        
+    def get_action(self, x):
+        vectors=[(1,1),(1,0),(0,1),(-1,-1),(-1,0),(0,-1),(-1,1),(1,-1)] #possible moves
+        arr=list(self.forward(x)[0])
+        ind=np.argmax(arr)
+        return vectors[ind]
+
+
+
+class Agent_Conv2D:
+    def __init__(self, num_input, layers, num_output):
+        assert type(layers)==type([]), "Error with layers, give array of the number of layers"
+        self.num_input = num_input  #set input number
+        self.num_output = num_output #set ooutput number
+        self.hidden=[]
+        last=num_input
+        self.num_genes=0
+        for layer in layers:
+            self.hidden.append(layer)
+            self.num_genes+=(last * layer)
+            last=layer
+        self.num_genes +=(self.hidden[-1]*num_output)+num_output
+        self.weights = None
+        self.hidden_weights=None
+        self.bias = None
+        print("Auto",self.num_genes)
+    def set_genes(self, gene):
+        weight_idxs = self.num_input * self.hidden[0] #size of weights to hidden
+        current=weight_idxs
+        weights_idxs=[current] #start with end of last
+        for i in range(len(self.hidden)-1):
+            current+=self.hidden[i]*self.hidden[i+1] #calculate next idx for each layer
+            weights_idxs.append(current)
+        bias_idxs=None
+        weights_idxs.append(self.hidden[-1] * self.num_output + weights_idxs[-1]) #add last layer heading to output
+        bias_idxs = weights_idxs[-1]+ self.num_output #sizes of biases
+        w = gene[0 : weight_idxs].reshape(self.hidden[0], self.num_input)   #merge genes
+        ws=[]
+        for i in range(len(self.hidden)-1):
+            ws.append(gene[weights_idxs[i] : weights_idxs[i+1]].reshape(self.hidden[i+1], self.hidden[i]))
+        ws.append(gene[weights_idxs[-2] : weights_idxs[-1]].reshape(self.num_output, self.hidden[-1]))
+        b = gene[weights_idxs[-1]: bias_idxs].reshape(self.num_output,) #merge genes
+
+        self.weights = torch.from_numpy(w) #assign weights
+        self.hidden_weights=[]
+        for w in ws:
+            self.hidden_weights.append(torch.from_numpy(w))
+        self.bias = torch.from_numpy(b) #assign biases
+
+    def forward(self, x, vec):
+        #create conv layer
+        scharr = np.array([[ -3-3, 0-10,  +3 -3],
+                   [-10+0, 0+ 0, +10 +0],
+                   [ -3+3, 0+10,  +3 +3]]) # Gx + j*Gy
+        x= signal.convolve2d(x, scharr, boundary='symm', mode='same')   
+        x=x.flatten()
+        vec=vec.astype(float)
+        x=np.concatenate((x,vec))
+        x=torch.tensor(x[:,np.newaxis]).float()  
+        #x = torch.tensor(np.dot(self.weights.float(),x).flatten()).float()
+        #run through forward layers
+        x = torch.mm(x.T, self.weights.T.float()) #first layer
+
+        for i in range(len(self.hidden_weights)-1):
+            x = torch.mm(x,self.hidden_weights[i].T.float()) #second layer
+        return torch.mm(x,self.hidden_weights[-1].T.float()) + self.bias #third layer
+    
+    def get_action(self, x,vec):
+        vec=np.array(vec)
+        vectors=[(1,1),(1,0),(0,1),(-1,-1),(-1,0),(0,-1),(-1,1),(1,-1)] #possible moves
+        arr=list(self.forward(x,vec)[0])
+        ind=np.argmax(arr)
+        return vectors[ind]
---
+++ Dev/BackBending/wheg.py
@@ -0,0 +1,184 @@
+from adafruit_servokit import ServoKit
+#import cv2
+import numpy as np
+
+
+kit=ServoKit(channels=16,address=0x44)
+class whegbot:
+    def __init__(self):
+        self.movingForward=False
+        self.movingBackward=False
+        kit.servo[15].angle=100
+        kit.servo[14].angle=100
+    def getBack(self):
+        return kit.servo[14].angle
+    def rightTurn(self):
+        #kit.continuous_servo[0].throttle = 1
+        #kit.continuous_servo[1].throttle = 1
+        #kit.continuous_servo[2].throttle = 1
+        #kit.continuous_servo[3].throttle = 1
+        try:
+            if kit.servo[15].angle+10<=180:
+                kit.servo[15].angle+=10
+        except:
+            pass
+        
+    def leftTurn(self):
+        #kit.continuous_servo[0].throttle = -1
+        #kit.continuous_servo[1].throttle = -1
+        #kit.continuous_servo[2].throttle = -1
+        #kit.continuous_servo[3].throttle = -1
+        print(kit.servo[15].angle)
+        try:
+            if kit.servo[15].angle-10>=0:
+                kit.servo[15].angle-=10
+        except:
+            pass
+
+    def forward(self):
+        self.movingForward=True
+        self.movingBackward=False
+        kit.continuous_servo[0].throttle = 1
+        kit.continuous_servo[1].throttle = -1
+        kit.continuous_servo[2].throttle = 1
+        kit.continuous_servo[3].throttle = -1
+
+    def backward(self):
+        self.movingForward=False
+        self.movingBackward=True
+        kit.continuous_servo[0].throttle = -1
+        kit.continuous_servo[1].throttle = 1
+        kit.continuous_servo[2].throttle = -1
+        kit.continuous_servo[3].throttle = 1
+
+    def stop(self):
+        print("Stop")
+        self.movingForward=False
+        self.movingBackward=False
+        kit.continuous_servo[0].throttle = 0
+        kit.continuous_servo[1].throttle = 0
+        kit.continuous_servo[2].throttle = 0
+        kit.continuous_servo[3].throttle = 0
+
+    def rotateUp(self):
+        try:
+            if kit.servo[14].angle+10<=180:
+                kit.servo[14].angle+=10
+        except:
+            pass
+    def rotateDown(self):
+        try:
+            if kit.servo[14].angle-10>=0:
+                kit.servo[14].angle-=10
+        except:
+            pass
+"""
+import time
+moving=False
+
+cap=cv2.VideoCapture(-1, cv2.CAP_V4L)
+
+filename = 'video.avi'
+frames_per_second = 24.0
+res = '720p'
+def change_res(cap, width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+# Standard Video Dimensions Sizes
+STD_DIMENSIONS =  {
+    "480p": (640, 480),
+    "720p": (1280, 720),
+    "1080p": (1920, 1080),
+    "4k": (3840, 2160),
+}
+
+
+# grab resolution dimensions and set video capture to it.
+def get_dims(cap, res='1080p'):
+    width, height = STD_DIMENSIONS["480p"]
+    if res in STD_DIMENSIONS:
+        width,height = STD_DIMENSIONS[res]
+    ## change the current caputre device
+    ## to the resulting resolution
+    change_res(cap, width, height)
+    return width, height
+
+# Video Encoding, might require additional installs
+# Types of Codes: http://www.fourcc.org/codecs.php
+VIDEO_TYPE = {
+    'avi': cv2.VideoWriter_fourcc(*'XVID'),
+    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
+    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+}
+
+def get_video_type(filename):
+    filename, ext = os.path.splitext(filename)
+    if ext in VIDEO_TYPE:
+      return  VIDEO_TYPE[ext]
+    return VIDEO_TYPE['avi']
+out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
+
+
+kit.servo[4].angle=90
+"""
+"""
+while 1:
+     try:
+        events = get_gamepad()
+     except:
+        stop()
+        raise OSError("...")
+     _,frame=cap.read()
+     out.write(frame)
+     for event in events:
+         #left stick
+         #print(event.code)
+         _,frame=cap.read()
+         out.write(frame)
+         if event.code=="BTN_TRIGGER_HAPPY1":
+             if moving:
+                 moving=False
+                 stop()
+             else:
+                moving=True
+                rightTurn()
+         elif event.code=="BTN_TRIGGER_HAPPY2":
+             if moving:
+                 moving=False
+                 stop()
+             else:
+                moving=True
+                leftTurn() 
+         elif event.code=="ABS_RZ":
+             if event.state==0:
+                 stop() 
+                 movingForward=False
+             else:
+                 forward()
+                 movingForward=True
+         elif event.code=="ABS_Z":
+             if event.state==0:
+                 stop()
+                 movingBackward=False
+             else:
+                 backward()
+                 movingBackward=True
+         elif event.code=="BTN_TRIGGER_HAPPY3":
+             rotateDown()
+         elif event.code=="BTN_TRIGGER_HAPPY4":
+             rotateUp()
+         #right stick
+         elif event.code == "ABS_RX":
+             print("right stick forward/backward", event.state)
+         elif event.code == "ABS_RY":
+             print("right stick turn", event.state)
+         elif event.code!="ABS_X" and event.code!="ABS_RZ" and event.code!="ABS_Z":
+             pass
+     if movingForward:
+        forward()
+     elif movingBackward:
+        backward()
+        
+stop()
+"""
